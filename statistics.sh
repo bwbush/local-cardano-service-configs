80a9556181df1efad81138912664f4a60db94f6a
Brian W Bush <code@functionally.io>
2022-01-10 11:25:17 -0700

    fixed port numbers

--- statistics.sh
+++ statistics.sh
@@ -10,5 +10,5 @@ du -h plutus-apps/plutus-pab/test-node/testnet/chain-index.db
 
 echo
 echo "Chain index size:"
-curl -H 'accept: application/json;charset=utf-8' http://localhost:39084/tip
+curl -H 'accept: application/json;charset=utf-8' http://localhost:39083/tip
 echo
