cd2f2e71ff92e2ed6aabd708c682e41f0ce4fe7c
Brian W Bush <code@functionally.io>
2022-01-27 08:59:47 -0700

    switched to makefile

--- launch-node.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd cardano-node
-
-nix-build -A scripts.testnet.node -o build-node-testnet
-
-./build-node-testnet/bin/cardano-node-testnet
--- launch-wallet.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd cardano-wallet
-
-nix-build -A cardano-wallet -o build-wallet
-
-./build-wallet/bin/cardano-wallet serve --testnet ../cardano-node/configuration/defaults/byron-testnet/genesis.json --port 38090 --node-socket ../cardano-node/state-node-testnet/node.socket
